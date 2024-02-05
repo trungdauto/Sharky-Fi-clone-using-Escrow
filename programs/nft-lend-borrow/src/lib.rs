e78027db0d0ec7952e248e80d421377fa86c7d2e
trungdauto <158998530+trungdauto@users.noreply.github.com>
2024-02-05 16:43:24 +0700

    declare program

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("4qwJ3RAGnsWt5ETUB5QipyPPHoSHEBSrzAYaChtvQQAQ");
 
 #[program]
 pub mod nft_lend_borrow {
